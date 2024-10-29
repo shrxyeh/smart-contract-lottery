7b61db93e9a1dd0ca4ff5c650f61822cfebda221
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-10-29 19:05:55 +0530

    Update RaffleTest.t.sol

--- test/uint/RaffleTest.t.sol
+++ test/uint/RaffleTest.t.sol
@@ -5,14 +5,16 @@ pragma solidity ^0.8.19;
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
-import {StdCheats} from "forge-std/StdCheats.sol";
-import {VRFCoordinatorV2Mock} from "../mocks/VRFCoordinatorV2Mock.sol";
-import {CreateSubscription} from "../../script/Interactions.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "../../test/mocks/LinkToken.sol";
+import {CodeConstants} from "../../script/HelperConfig.s.sol";
 
-contract RaffleTest is StdCheats, Test {
-    /* Errors */
+contract RaffleTest is Test, CodeConstants {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
     event RequestedRaffleWinner(uint256 indexed requestId);
     event RaffleEnter(address indexed player);
     event WinnerPicked(address indexed player);
@@ -20,42 +22,51 @@ contract RaffleTest is StdCheats, Test {
     Raffle public raffle;
     HelperConfig public helperConfig;
 
-    uint64 subscriptionId;
+    uint256 subscriptionId;
     bytes32 gasLane;
     uint256 automationUpdateInterval;
     uint256 raffleEntranceFee;
     uint32 callbackGasLimit;
-    address vrfCoordinatorV2;
+    address vrfCoordinatorV2_5;
+    LinkToken link;
 
     address public PLAYER = makeAddr("player");
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    uint256 public constant LINK_BALANCE = 100 ether;
 
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.run();
         vm.deal(PLAYER, STARTING_USER_BALANCE);
 
-        (
-            ,
-            gasLane,
-            automationUpdateInterval,
-            raffleEntranceFee,
-            callbackGasLimit,
-            vrfCoordinatorV2, // link
-            // deployerKey
-            ,
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        subscriptionId = config.subscriptionId;
+        gasLane = config.gasLane;
+        automationUpdateInterval = config.automationUpdateInterval;
+        raffleEntranceFee = config.raffleEntranceFee;
+        callbackGasLimit = config.callbackGasLimit;
+        vrfCoordinatorV2_5 = config.vrfCoordinatorV2_5;
+        link = LinkToken(config.link);
 
-        ) = helperConfig.activeNetworkConfig();
+        vm.startPrank(msg.sender);
+        if (block.chainid == LOCAL_CHAIN_ID) {
+            link.mint(msg.sender, LINK_BALANCE);
+            VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fundSubscription(
+                subscriptionId,
+                LINK_BALANCE
+            );
+        }
+        link.approve(vrfCoordinatorV2_5, LINK_BALANCE);
+        vm.stopPrank();
     }
 
     function testRaffleInitializesInOpenState() public view {
         assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
 
-    /////////////////////////
-    // enterRaffle         //
-    /////////////////////////
-
+    /*//////////////////////////////////////////////////////////////
+                              ENTER RAFFLE
+    //////////////////////////////////////////////////////////////*/
     function testRaffleRevertsWHenYouDontPayEnought() public {
         // Arrange
         vm.prank(PLAYER);
@@ -98,9 +109,9 @@ contract RaffleTest is StdCheats, Test {
         raffle.enterRaffle{value: raffleEntranceFee}();
     }
 
-    /////////////////////////
-    // checkUpkeep         //
-    /////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                              CHECKUPKEEP
+    //////////////////////////////////////////////////////////////*/
     function testCheckUpkeepReturnsFalseIfItHasNoBalance() public {
         // Arrange
         vm.warp(block.timestamp + automationUpdateInterval + 1);
@@ -111,7 +122,6 @@ contract RaffleTest is StdCheats, Test {
 
         // Assert
         assert(!upkeepNeeded);
-        
     }
 
     function testCheckUpkeepReturnsFalseIfRaffleIsntOpen() public {
@@ -157,10 +167,9 @@ contract RaffleTest is StdCheats, Test {
         assert(upkeepNeeded);
     }
 
-    /////////////////////////
-    // performUpkeep       //
-    /////////////////////////
-
+    /*//////////////////////////////////////////////////////////////
+                             PERFORMUPKEEP
+    //////////////////////////////////////////////////////////////*/
     function testPerformUpkeepCanOnlyRunIfCheckUpkeepIsTrue() public {
         // Arrange
         vm.prank(PLAYER);
@@ -207,13 +216,12 @@ contract RaffleTest is StdCheats, Test {
         Raffle.RaffleState raffleState = raffle.getRaffleState();
         // requestId = raffle.getLastRequestId();
         assert(uint256(requestId) > 0);
-        assert(uint(raffleState) == 1); // 0 = open, 1 = calculating
+        assert(uint256(raffleState) == 1); // 0 = open, 1 = calculating
     }
 
-    /////////////////////////
-    // fulfillRandomWords //
-    ////////////////////////
-
+    /*//////////////////////////////////////////////////////////////
+                           FULFILLRANDOMWORDS
+    //////////////////////////////////////////////////////////////*/
     modifier raffleEntered() {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: raffleEntranceFee}();
@@ -236,16 +244,15 @@ contract RaffleTest is StdCheats, Test {
     {
         // Arrange
         // Act / Assert
-        vm.expectRevert("nonexistent request");
+        vm.expectRevert(VRFCoordinatorV2_5Mock.InvalidRequest.selector);
         // vm.mockCall could be used here...
-        VRFCoordinatorV2Mock(vrfCoordinatorV2).fulfillRandomWords(
+        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(
             0,
             address(raffle)
         );
 
-        vm.expectRevert("nonexistent request");
-
-        VRFCoordinatorV2Mock(vrfCoordinatorV2).fulfillRandomWords(
+        vm.expectRevert(VRFCoordinatorV2_5Mock.InvalidRequest.selector);
+        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(
             1,
             address(raffle)
         );
@@ -279,9 +286,10 @@ contract RaffleTest is StdCheats, Test {
         vm.recordLogs();
         raffle.performUpkeep(""); // emits requestId
         Vm.Log[] memory entries = vm.getRecordedLogs();
+        console2.logBytes32(entries[1].topics[1]);
         bytes32 requestId = entries[1].topics[1]; // get the requestId from the logs
 
-        VRFCoordinatorV2Mock(vrfCoordinatorV2).fulfillRandomWords(
+        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(
             uint256(requestId),
             address(raffle)
         );
